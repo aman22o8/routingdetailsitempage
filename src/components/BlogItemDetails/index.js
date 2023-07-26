938d6dbc09f34c63e4349d81c9d202bffff4d20a
aman2208 <amankumar.kumar369@gamail.com>
2023-07-26 20:56:58 +0000

    initial code

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,63 @@
+import {Component} from 'react'
+
+import './index.css'
+import {async} from 'rxjs'
+
+// const blogData = {
+//   title: 'Blog Name',
+//   imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-3-img.png',
+//   avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//   author: 'Author Name',
+//   content:
+//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+// }
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}}
+
+  componentDidMount() {
+    this.getItemsDetails()
+  }
+
+  getItemsDetails = async () => {
+    console.log(this.props)
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    console.log(data)
+    const updatedDetails = {
+      title: data.topic,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+      content: data.content,
+    }
+    this.setState({blogData: updatedDetails})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
